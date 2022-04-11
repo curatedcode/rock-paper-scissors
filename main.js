b2180cb7af6481d1303571098195d2f8ec5da0f8
curatedcode <zackaryfotheringham@hotmail.com>
2022-04-11 10:08:58 -0400

    added game function to play 5 rounds

--- main.js
+++ main.js
@@ -1,32 +1,67 @@
+'use strict';
+
 function computerPlay() {
     const rps = ['rock', 'paper', 'scissors'];
     const randomPick = rps[Math.random() * rps.length | 0];
     return(randomPick);
 }
 
-const unmodifiedPlayerSelection = 'RoCk';
-playerSelection = unmodifiedPlayerSelection.toLowerCase();
 const computerSelection = computerPlay();
+console.log(computerSelection);
+
+const entryPrompt = 'paper';
+const playerSelection = entryPrompt.toLowerCase();
+
+let cScore = 0;
+let pScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     const pBR = 'Paper beats Rock!';
     const sBP = 'Scissors beats Paper!';
     const rBS = 'Rock beats Scissors!';
+    const tie = 'It\'s a tie';
+    const cWin = 'Computer Wins! You Suck!';
+    const pWin = 'You Win! Computers Suck!';
+    const fucked = 'Something\'s fucked up man!';
     if (playerSelection != '' && computerSelection != '') {
         if (playerSelection == 'rock' && computerSelection == 'paper') {
-            return(`${pBR}`);
+            cScore++;
+            return(`${pBR} ${cWin}`);
         } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-            return(`${rBS}`);
+            cScore++;
+            return(`${rBS} ${cWin}`);
         } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-            return(`${sBP}`);
+            cScore++;
+            return(`${sBP} ${cWin}`);
         } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-            return(`${pBR}`);
+            pScore++;
+            return(`${pBR} ${pWin}`);
         } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-            return(`${sBP}`);
+            pScore++;
+            return(`${sBP} ${pWin}`);
         } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-            return(`${rBS}`);
+            pScore++;
+            return(`${rBS} ${pWin}`);
+        } else if (playerSelection == computerSelection) {
+            return(tie);
+        } else {
+            return(fucked);
         }
     }
 }
 
-playRound(playerSelection, computerSelection);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        entryPrompt;
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (pScore < cScore) {
+        console.log('You Lose! Can\'t believe you suck that bad!')
+    } else if (pScore > cScore) {
+        console.log('You win! You\'re smarter than a computer!')
+    } else if (pScore == cScore) {
+        console.log('We\'ll looks like you\'re as smart as a computer!');
+    }
+}
+
+game();
